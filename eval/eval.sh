c91267571e4e811375e796e640388ede21dd2005
Moonite <moonite1209@qq.com>
2024-05-20 13:23:17 +0800

    eval

--- eval/eval.sh
+++ eval/eval.sh
@@ -11,7 +11,8 @@ python evaluate_iou_loc.py \
         --feat_dir ${root_path}/output \
         --ae_ckpt_dir ${root_path}/ckpt \
         --output_dir ${root_path}/eval_result \
+        --json_folder ${gt_folder} \
         --mask_thresh 0.4 \
         --encoder_dims 256 128 64 32 3 \
-        --decoder_dims 16 32 64 128 256 256 512 \
-        --json_folder ${gt_folder}
+        --decoder_dims 16 32 64 128 256 256 512 
+        
