41f2be1a3d55eeb3fcdcf8eea946df58b29c3142
Moonite <moonite1209@qq.com>
2024-05-22 15:50:05 +0800

    eval

--- eval/evaluate_iou_loc.py
+++ eval/evaluate_iou_loc.py
@@ -17,8 +17,8 @@ import time
 from tqdm import tqdm
 
 import sys
-sys.path.append("..")
-sys.path.append(sys.path.append(os.path.join(os.path.dirname(__file__),"..")))
+# sys.path.append("..")
+sys.path.append(os.path.join(os.path.dirname(__file__),".."))
 import colormaps
 from autoencoder.model import Autoencoder
 from openclip_encoder import OpenCLIPNetwork
