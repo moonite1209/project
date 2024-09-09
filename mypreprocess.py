3771e3b39d075307da929a012cf1f96a9556616b
moonite <moonite1209@qq.com>
2024-09-09 17:22:01 +0800

    sam2

--- mypreprocess.py
+++ mypreprocess.py
@@ -1,6 +1,8 @@
 import os
+import random
 import shutil
 import io
+import argparse
 from typing import List, Sequence
 import matplotlib.axes
 import numpy as np
@@ -11,6 +13,7 @@ import torch
 import torchvision
 from sam2.sam2_video_predictor import SAM2VideoPredictor
 from sam2.automatic_mask_generator import SAM2AutomaticMaskGenerator
+import cv2
 
 def show_mask(mask, ax: matplotlib.axes.Axes, obj_id=None, random_color=False):
     ax.clear()
@@ -92,24 +95,82 @@ def mask():
         mask=mask['segmentation']
         color = np.random.randint((256,256,256), dtype=np.uint8)
         smap[mask]=color
-    # plt.imshow(smap)
-    # plt.show()
     track([m['segmentation'] for m in masks])
 
-def main() -> None:
-    # for file in os.listdir('data/lerf'):
-    #     if file=='label':
-    #         continue
-    #     src = os.path.join('data/lerf', file, 'images')
-    #     dst = os.path.join('data/lerf', file, 'input')
-    #     if os.path.exists(dst):
-    #         shutil.rmtree(dst)
-    #     os.makedirs(dst)
-    #     images = os.listdir(src)
-    #     images.sort()
-    #     for index, file in enumerate(images):
-    #         with open(os.path.join(src, file), 'rb') as x,open(os.path.join(dst, f'{str(index).rjust(5,'0')}.jpg'), 'wb') as y:
-    #             y.write(x.read())
-    mask()
+def video_segment(images, mask_generator, predictor):
+    pass
+def extract_semantics(images, mask_generator, predictor, save_folder):
+    pass
 
-main()
+def seed_everything(seed_value):
+    random.seed(seed_value)
+    np.random.seed(seed_value)
+    torch.manual_seed(seed_value)
+    os.environ['PYTHONHASHSEED'] = str(seed_value)
+    
+    if torch.cuda.is_available(): 
+        torch.cuda.manual_seed(seed_value)
+        torch.cuda.manual_seed_all(seed_value)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = True
+
+def prepare_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset_path', '-s', type=str, required=True)
+    parser.add_argument('--resolution', type=int, default=-1)
+    parser.add_argument('--image_folder', type=str, default='input')
+    parser.add_argument('--save_folder', type=str, default='semantic')
+    parser.add_argument('--sam_path', type=str, default="facebook/sam2-hiera-large")
+    torch.set_default_dtype(torch.float32)
+    return parser.parse_args()
+
+def main() -> None:
+    seed_everything(42)
+    args = prepare_args()
+    image_path = os.path.join(args.dataset_path, args.image_folder)
+    mask_generator = SAM2AutomaticMaskGenerator.from_pretrained(args.sam_path, 
+                                                                # points_per_side=32,
+                                                                # pred_iou_thresh=0.7,
+                                                                # box_nms_thresh=0.7,
+                                                                # stability_score_thresh=0.85,
+                                                                # crop_n_layers=1,
+                                                                # crop_n_points_downscale_factor=1,
+                                                                min_mask_region_area=100
+                                                                )
+    predictor = SAM2VideoPredictor.from_pretrained(args.sam_path)
+    img_list = []
+    WARNED = False
+    images = [os.path.join(image_path, p) for p in os.listdir(image_path)]
+    images.sort()
+    for image_path in images:
+        image = cv2.imread(image_path)
+
+        orig_w, orig_h = image.shape[1], image.shape[0]
+        if args.resolution == -1:
+            if orig_h > 1080:
+                if not WARNED:
+                    print("[ INFO ] Encountered quite large input images (>1080P), rescaling to 1080P.\n "
+                        "If this is not desired, please explicitly specify '--resolution/-r' as 1")
+                    WARNED = True
+                global_down = orig_h / 1080
+            else:
+                global_down = 1
+        else:
+            global_down = orig_w / args.resolution
+            
+        scale = float(global_down)
+        resolution = (int( orig_w  / scale), int(orig_h / scale))
+        
+        image = cv2.resize(image, resolution)
+        image = torch.from_numpy(image)
+        img_list.append(image)
+    images = [img.permute(2, 0, 1)[None, ...] for img in img_list]
+    images = torch.cat(images)
+
+    save_folder = os.path.join(args.dataset_path, args.save_folder)
+    os.makedirs(save_folder, exist_ok=True)
+    video_segment(images, mask_generator, predictor)
+    extract_semantics(images, mask_generator, predictor, save_folder)
+
+if __name__  == '__main__':
+    main()
