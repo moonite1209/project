17194e2fcf880604767e506d482a415a953d2ca6
moonite <moonite1209@qq.com>
2024-09-29 16:03:36 +0800

    sam2

--- mypreprocess.py
+++ mypreprocess.py
@@ -383,27 +383,24 @@ def main() -> None:
     for image_name in image_names:
         image = cv2.imread(os.path.join(image_path, image_name))
 
-        # orig_w, orig_h = image.shape[1], image.shape[0]
-        # if args.resolution == -1:
-        #     if orig_h > 1080:
-        #         if not WARNED:
-        #             print("[ INFO ] Encountered quite large input images (>1080P), rescaling to 1080P.\n "
-        #                 "If this is not desired, please explicitly specify '--resolution/-r' as 1")
-        #             WARNED = True
-        #         global_down = orig_h / 1080
-        #     else:
-        #         global_down = 1
-        # else:
-        #     global_down = orig_w / args.resolution
+        orig_w, orig_h = image.shape[1], image.shape[0]
+        if args.resolution == -1:
+            if orig_h > 1080:
+                if not WARNED:
+                    print("[ INFO ] Encountered quite large input images (>1080P), rescaling to 1080P.\n "
+                        "If this is not desired, please explicitly specify '--resolution/-r' as 1")
+                    WARNED = True
+                global_down = orig_h / 1080
+            else:
+                global_down = 1
+        else:
+            global_down = orig_w / args.resolution
             
-        # scale = float(global_down)
-        # resolution = (int( orig_w  / scale), int(orig_h / scale))
-        
-        # image = cv2.resize(image, resolution)
-        # image = torch.from_numpy(image)
+        scale = float(global_down)
+        resolution = (int( orig_w  / scale), int(orig_h / scale))
+
+        image = cv2.resize(image, resolution)
         img_list.append(image)
-    # images = [img[None, ...] for img in img_list]
-    # images = torch.cat(images).cuda() # 0.5G
     images = np.stack(img_list)
 
     os.makedirs(save_path, exist_ok=True)
